45e6d625624d4992d75a475247d50bff148c13e2
Mauricio Casagrande <mauricio.araras@hotmail.com>
2021-06-01 19:32:01 -0300

    Att

--- site/request.php
+++ site/request.php
@@ -35,6 +35,6 @@
     echo "<b>Curso: </b>".$curso."<br>";
     echo "<b>Média: </b>".$media."<br>";
     echo "<b>Presença: </b>".$presenca."%"."<br>";
-    echo "<b>Situação: </b>".$aprovacao;
+    echo "<b>Situação: </b>".$aprovacao."<br>";
 
 ?>
